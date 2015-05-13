aef4b3786f5b8a2474ab8f30d30abc724c39615b
Edward Cragg <edward.cragg@codethink.co.uk>
2015-05-13 17:05:16 +0000

    T: make test.sh "portable"

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 export DISK_SIZE=3G
 export PARTITION_FILE='rocketboard/partitions'
 export PYTHONPATH=`pwd`
-python morphlib/exts/rawdisk.write temproot testnew.img
+python morphlib/exts/rawdisk.write $1 testnew.img
