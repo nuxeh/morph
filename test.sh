8a9dbace6d19977ad6613a366d45264e2f4955d0
Edward Cragg <edward.cragg@codethink.co.uk>
2015-05-18 15:29:05 +0100

    T: use 6G image for testing

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export DISK_SIZE=3G
+export DISK_SIZE=6G
 export PARTITION_FILE='rocketboard/partitions'
 export PYTHONPATH=`pwd`
+#export 
 python morphlib/exts/rawdisk.write $1 testnew.img
