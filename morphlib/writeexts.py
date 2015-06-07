eaedc60812e9c495bfa55cf471357725d50b7b6d
Edward Cragg <edward.cragg@codethink.co.uk>
2015-06-07 13:09:03 +0100

    rawdisk.write: Add function to find device names for partitions
    This function infers the name given by the system to a partition on a
    block device, given the number of the desired partition, and the name
    of the block device.
    
    E.g. Return '/dev/sda1' when asked for the first partition on '/dev/sda'
         Return '/dev/mmcblk1p2' for the second partition on '/dev/mmcblk1'
    
    Change-Id: Ibc05940e73c86fa485c12eacca93a633d24c3c15

--- morphlib/writeexts.py
+++ morphlib/writeexts.py
@@ -649,6 +649,21 @@ class WriteExtension(cliapp.Application):
             raise cliapp.AppException('Can\'t get physical sector '
                                       'size for %s' % location)
 
+    def get_part_devname(self, location, partition_number):
+        ''' Get the name of the device node
+            given to a partition on a block device '''
+
+        if self.is_device(location):
+            if re.match('.*[hs]d[a-p]', location):
+                return location + str(partition_number)
+            elif re.match('.*mmcblk\d+', location):
+                return location + 'p' + str(partition_number)
+            else:
+                self.status(msg='FIXME: Device name not implemented')
+                raise
+        else:
+            raise cliapp.AppException('Not a device!')
+
     def do_partitioning(self, location, temp_root, part_file):
         ''' The steps required to create a partitioned device or
             device image
